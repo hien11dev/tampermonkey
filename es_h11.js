7043754e6ef1eccda1ce30d5b0cc21894b2e01c4
Hiển <58682524+hien11dev@users.noreply.github.com>
2021-10-16 19:42:26 +0700

    Create es_h11.js

---
+++ es_h11.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         ES_H11
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @include        http://*
+// @include        https://*
+// @icon         https://cdn.icon-icons.com/icons2/798/PNG/512/5_icon-icons.com_65772.png
+// @grant        none
+// @run-at         document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('.odds-button').remove();
+})();
