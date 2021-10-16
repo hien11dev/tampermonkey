aea6900144a11728075aba4c04d7db1454615453
Hiển <58682524+hien11dev@users.noreply.github.com>
2021-10-16 19:52:14 +0700

    Update es_h11.js

--- es_h11.js
+++ es_h11.js
@@ -1,15 +1,22 @@
 // ==UserScript==
 // @name         ES_H11
-// @namespace    http://tampermonkey.net/
+// @namespace    H11
+// @author       H11
+// @copyright    H11
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  tampermonkey
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @include        http://*
-// @include        https://*
 // @icon         https://cdn.icon-icons.com/icons2/798/PNG/512/5_icon-icons.com_65772.png
-// @grant        none
-// @run-at         document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @include      http://*
+// @include      https://*
+// @match        *://*/*
+// @downloadURL  
+// @updateURL     
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
